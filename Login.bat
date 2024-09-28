a7149a93d8d1d89d7802abf42928fedc6915fd35
Chunn241529 <133483726+Chunn241529@users.noreply.github.com>
2024-09-29 00:43:52 +0700

    fix

--- Login.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-REM <EDIT YOUR LOGIN INFORMATION HERE> 
-SET account=abc
-SET password=abc
-REM <EDIT YOUR LOGIN INFORMATION HERE> 
-
-cd bin64
-start BlackDesert64.exe %account%,%password%
