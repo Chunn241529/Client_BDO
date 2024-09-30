311a2c4c04c226b2fb36642e4db40f67a86ebfe8
Chunn241529 <133483726+Chunn241529@users.noreply.github.com>
2024-09-30 13:55:52 +0700

    update

--- Login.bat
+++ Login.bat
@@ -1,9 +1,7 @@
 @echo off
-
-REM <EDIT YOUR LOGIN INFORMATION HERE> 
+REM <EDIT YOUR LOGIN INFORMATION HERE>
 SET account=abc
 SET password=abc
-REM <EDIT YOUR LOGIN INFORMATION HERE> 
-
+REM <EDIT YOUR LOGIN INFORMATION HERE>
 cd bin64
-start BlackDesert64.exe %account%,%password%
+start BlackDesert64.exe abc,abc
